1f190ad72a3335d2e94194e67a12fc526e820efa
Irina Mykhailova <irina.serga.mykhailova@gmail.com>
2025-02-15 17:43:57 +0200

    add

--- src/com/dmdev/oop/lesson2/Computer.java
+++ src/com/dmdev/oop/lesson2/Computer.java
@@ -4,12 +4,55 @@ public class Computer {
     //не указан модификатор доступа, по умолчанию используется package-private (default access).
     //package-private (default): Переменная будет доступна только внутри текущего пакета.
     //Она не будет доступна вне пакета, даже если класс унаследован.
-    int ssd = 500; // дефолтное значение
-    int ram = 1024;
+    int ssd = 500; // дефолтное значение - состояние объекта
+    int ram;  // default = 0
+//    long ram;
     //статические переменные загрузятся первыми при выполнении
     //функции = методы
+
+    Computer (){
+        System.out.println("Computer Constructor");
+    }
+
+    Computer(int newSSD) {
+        ssd = newSSD;
+        load();
+
+    }
+
+//    Computer(int newSSD, int newRAM) {
+//        ssd = newSSD;
+//        ram = newRAM;
+//        System.out.println(" ");
+//    }
+
+    Computer(int ssd, int newRAM) {
+        // конфликт имен
+        // здесь приоритет у параметра в методе выше
+        this.ssd = ssd; // ссылка на текущий объект
+        ram = newRAM;
+        System.out.println(" ");
+    }
+// влияет не только кол-во параметров, но и их порядок
+//    Computer(long newRAM, int ssd) {
+//        // конфликт имен
+//        // здесь приоритет у параметра в методе выше
+//        this.ssd = ssd; // ссылка на текущий объект
+//        ram = newRAM;
+//        System.out.println(" ");
+//    }
+
+
+    // Сигнатура метода: название + список параметров. Возвращаемый тип не входит в сигнатуру метода!(void)
     void  load(){
         System.out.println("Loading computer...");
+    }
+
+    void  load(boolean open){
+        System.out.println("Loading computer...");
+        if (open){
+            System.out.println("Opening computer...");
+        }
     }
 
     void printState(){
--- src/com/dmdev/oop/lesson2/ComputerRunner.java
+++ src/com/dmdev/oop/lesson2/ComputerRunner.java
@@ -6,10 +6,21 @@ public class ComputerRunner {
         int value = 5; //тип данных именование = значение
         // класс - это новый тип, который мы создали
         Computer computer = new Computer(); //переменная компьютер с типом компьютер //оператор new - ключевое слово, а затем конструктор
-        computer.load();
-        System.out.println("Ram: " + computer.ram);
-        System.out.println("Ssd: " + computer.ssd);
+        computer.printState();
+//        computer.load();
+//        System.out.println("Ram: " + computer.ram);
+//        System.out.println("Ssd: " + computer.ssd);
 
         //new Computer(); -- конструктор по-умолчанию. Ф-я, кот должна точь в точь соответствовать именованию класса
+
+        Computer computer2 = new Computer(1050);
+        computer2.printState();
+
+        Computer computer3 = new Computer(10000, 10000);
+        computer3.printState();
+        computer3.load(true);
     }
+
+
+
 }
